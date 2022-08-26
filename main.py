d63e3084d177fc12b9bc607c4d68db256e37a64a
Roman <romanschneider9@gmail.com>
2022-08-26 15:13:36 +0300

    functions refactored

--- main.py
+++ main.py
@@ -41,11 +41,13 @@ def check_username():
 def generate_password(length: int = 15):
     source = string.ascii_letters + string.digits
     password = "".join((random.choice(source) for _ in range(length)))
+    print("Password generated successfully.")
 
     return password
 
 
 def register_user(email: str, username: str, password: str):
+
     driver.get(REGISTER_URL)
 
     email_field = driver.find_element(By.NAME, "email")
@@ -69,8 +71,10 @@ def register_user(email: str, username: str, password: str):
     year.send_keys(Keys.ENTER)
     year.send_keys(Keys.ENTER)
 
+    time.sleep(2)
 
-def solve_captcha(site_key: str, url: str):
+
+def solve_captcha(site_key: str, url: str) -> dict:
     api_key = os.getenv('API_KEY')
 
     solver = TwoCaptcha(api_key)
@@ -89,38 +93,47 @@ def solve_captcha(site_key: str, url: str):
         return result
 
 
+def bypass_captcha(captcha_code: str):
+    try:
+        driver.execute_script(
+            f"document.querySelector('iframe').parentElement."
+            f"parentElement.__reactProps$.children"
+            f".props.onVerify('{captcha_code}')"
+        )
+        WebDriverWait(driver, 10).until(
+            EC.url_to_be("https://discord.com/channels/@me")
+        )
+
+    except Exception:
+        print("Oops, this email is already registered. Try other one")
+
+
+def get_token():
+    token = driver.execute_script(
+        "location.reload();var i=document.createElement('iframe');"
+        "document.body.appendChild(i);return "
+        "i.contentWindow.localStorage.token"
+    ).strip('"')
+
+    print("token:", token)
+
+
 def main():
     email = check_email()
     username = check_username()
     password = generate_password(15)
 
-    register_user(email, username, password)
-
-    solved_captcha = solve_captcha(SITE_KEY, REGISTER_URL)
-
-    if solved_captcha:
-        captcha_code = solved_captcha["code"]
+    try:
+        register_user(email, username, password)
+    except Exception as e:
+        print(e)
+    else:
         try:
-            driver.execute_script(
-                f"document.querySelector('iframe').parentElement."
-                f"parentElement.__reactProps$.children"
-                f".props.onVerify('{captcha_code}')"
-            )
-            WebDriverWait(driver, 10).until(
-                EC.url_to_be("https://discord.com/channels/@me")
-            )
-
-        except Exception:
-            print("Something went wrong... Try again!")
-
-        else:
-            token = driver.execute_script(
-                "location.reload();var i=document.createElement('iframe');"
-                "document.body.appendChild(i);return "
-                "i.contentWindow.localStorage.token"
-            ).strip('"')
-
-            print("token:", token)
+            captcha = solve_captcha(SITE_KEY, REGISTER_URL)
+            bypass_captcha(captcha["code"])
+            get_token()
+        except AttributeError:
+            print("Please try again")
 
 
 if __name__ == '__main__':
